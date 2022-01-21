65daa9ec2887e6159f76a9ca00f83b911c592142
Kayla Sade <sade@Kaylas-MacBook-Pro.local>
2022-01-21 15:01:27 -0500

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 
 import AddForm from './components/AddForm';
 import SmurfList from './components/SmurfList';
 import Header from './components/Header';
 
+import { connect } from 'react-redux';
+import { fetchSmurfs } from "./actions";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
-const App = ()=> {
+const App = ({fetchSmurfs})=> {
+
+  useEffect(()=> {
+    fetchSmurfs();
+  }, []);
+
   return (
     <div className="App">
       <Header />
@@ -20,8 +28,8 @@ const App = ()=> {
   );
 }
 
-export default App;
+export default connect(null, {fetchSmurfs})(App);
 
 //Task List:
-//1. Connect the fetchSmurfs actions to the App component.
-//2. Call the fetchSmurfs action when the component mounts.
+//1. [X]Connect the fetchSmurfs actions to the App component.
+//2. [X]Call the fetchSmurfs action when the component mounts.
